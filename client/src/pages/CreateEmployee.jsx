b2a9bfceb5c3225fe1bb4ac6d261a3518f27bf06
migeuu <miguelmarkiori@gmail.com>
2022-07-02 16:13:16 -0300

    creating form to create employee

--- client/src/pages/CreateEmployee.jsx
+++ client/src/pages/CreateEmployee.jsx
@@ -1,23 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { Header } from "../components/Header";
+import { validate } from "gerador-validador-cpf";
 
 export function CreateEmployee() {
+  const [employeeData, setEmployeeData] = useState({
+    status: "",
+    createdAt: new Date().toLocaleDateString(),
+    jobTitle: "",
+    cpf: "",
+    name: "",
+    state: "",
+    salary: "",
+  });
+
+  const handleChange = (event) => {
+    setEmployeeData({
+      ...employeeData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
   return (
     <div id="search-content">
       <Header title={"Criar um funcionário "} />
       <main>
-        {/* <form>
+        <form>
           <input
             type="text"
-            value={valueInput}
-            onChange={({ target }) => setValueInput(target.value)}
+            name="name"
+            value={employeeData.name}
+            onChange={handleChange}
+            required
           />
-          <button disabled={desabled} onClick={getDate}>
-            Pesquisar
-          </button>
+          <input
+            type="text"
+            name="CPF"
+            value={employeeData.cpf}
+            onChange={({ e }) => {
+              const validCpf = validate(e.target.value);
+              if (validCpf) {
+                handleChange(e);
+              }
+            }}
+            required
+          />
+          <input
+            type="text"
+            name="jobTitle"
+            value={employeeData.jobTitle}
+            onChange={handleChange}
+            required
+          />
         </form>
-        {typeof data === "string" ? <p> {data} </p> : ""}
-        {typeof data[0] === "object" ? <List employees={data} /> : ""} */}
       </main>
     </div>
   );
